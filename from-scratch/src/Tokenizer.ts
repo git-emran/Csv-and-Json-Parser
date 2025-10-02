047053b91f54a228017c282246354d2ff228752a
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-10-03 01:20:25 +0600

    handling brackets, colons, comma, strings parsing

--- from-scratch/src/Tokenizer.ts
+++ from-scratch/src/Tokenizer.ts
@@ -1,5 +1,59 @@
 import type { Token } from "./types.js";
 
 export const Tokenizer = (input: string): Token[] => {
-  return [];
+  let current = 0;
+  const tokens: Token[] = [];
+
+  while (current < input.length) {
+    let char = input[current];
+
+    if (char === "{") {
+      tokens.push({ type: "BraceOpen", value: char });
+      current++;
+      continue;
+    }
+
+    if (char === "}") {
+      tokens.push({ type: "BraceClose", value: char });
+      current++;
+      continue;
+    }
+
+    if (char === "[") {
+      tokens.push({ type: "BracketOpen", value: char });
+      current++;
+      continue;
+    }
+
+    if (char === "]") {
+      tokens.push({ type: "BracketClose", value: char });
+      current++;
+      continue;
+    }
+
+    if (char === ":") {
+      tokens.push({ type: "Colon", value: char });
+      current++;
+      continue;
+    }
+
+    if (char === ",") {
+      tokens.push({ type: "Comma", value: char });
+      current++;
+      continue;
+    }
+
+    if (char === '"') {
+      let value = "";
+      char = input[++current];
+
+      while (char !== '"') {
+        value += char;
+        char = input[++current];
+      }
+      current++;
+      tokens.push({ type: "String", value });
+      continue;
+    }
+  }
 };
